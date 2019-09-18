7e94a72154a2e95be72dfa4f202fcf64848c6cc5
Yana <yolandatwin87@gmail.com>
2019-09-18 19:26:36 +0300

    second
    second

---
+++ java/src/test/java/second.java
@@ -0,0 +1,30 @@
+import jdk.jfr.StackTrace;
+import org.openqa.selenium.Webdriver;
+import org.openqa.selenium.suppirt.ui.Webdriverwati;
+
+
+public class second {
+
+    private Webdriver driver;
+    private WebdriverWait wait;
+
+    @Before
+    public void start () {
+        driver = new Chromedriver();
+        wait = new Webdriverwait (driver,timeOutSeconds 10 );
+    }
+}
+    @Test
+     public void second () {
+    driver.get("https://bash.im/search");
+    driver.findElement(By.name("text")).sendKeys ("1");
+    driver.findElement(By.targname ("button")).click();
+    driver.until(titleIs("Цитата №1 -Цитатник Рунета"));
+    }
+   @After
+   public void stop() {
+    driver.quit();
+    driver = null;
+   }
+
+}
